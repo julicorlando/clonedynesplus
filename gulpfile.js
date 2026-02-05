3466c8d038a51a99c68aac390a95d15647bd368a
Julio <falecomjulioorlando@outlook.com>
2026-02-05 08:43:44 -0300

    Criando o hero e importando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const saas = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(saas({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function (){
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
